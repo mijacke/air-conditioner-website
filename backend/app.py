b294f0afb214fef4c18e1c189fe50b9f502bbde7
Mario Lassu <lassu@stud.uniza.sk>
2025-02-24 00:13:33 +0100

    Fixed BE and FE communiccation

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://air-conditioners-website.vercel.app"}})
+CORS(app, resources={r"/api/*": {"origins": "https://air-conditioners-website.vercel.app"}})
 
 # Add a default route for testing
 @app.route("/", methods=["GET"])
